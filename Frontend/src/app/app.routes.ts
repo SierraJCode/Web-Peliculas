e87bbdc1ffd171fbdb2febd18c889e1b6667d57e
Jesus Sierra <sierraja06@gmail.com>
2024-07-04 02:33:37 -0500

    Add primera ruta

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'/',
+        component: MoviesListComponent
+    }
+];
