a8da182458d0ba0d96152abd48e767934d23df6b
Jesus Sierra <sierraja06@gmail.com>
2024-07-04 05:24:50 -0500

    Creando metodos en los servicios

--- Frontend/src/app/services/movies.service.ts
+++ Frontend/src/app/services/movies.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Movie } from '../interfaces/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMovies(){
+
+  }
+
+  getMovie(id: string){
+
+  }
+
+  createMovie(movie: Movie){
+
+  }
+
+  updateMovie(id: string, movie: Movie){
+
+  }
+
+  deleteMovie(id: string){
+
+  }
+
 }
