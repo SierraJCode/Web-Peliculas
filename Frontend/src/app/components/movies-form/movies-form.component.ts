446149383fdebf90e325ac25d5c2be9b6085705c
Jesus Sierra <sierraja06@gmail.com>
2024-07-06 08:58:46 -0500

    Logica form lista

--- Frontend/src/app/components/movies-form/movies-form.component.ts
+++ Frontend/src/app/components/movies-form/movies-form.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Movie } from '../../interfaces/movie';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movies-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './movies-form.component.html',
   styleUrl: './movies-form.component.css'
 })
 export class MoviesFormComponent {
+
+  constructor(private moviesService: MoviesService){}
+
+  movie: Movie = {
+    title: '',
+    description: '',
+    producer: '',
+    time: '',
+    image: ''
+  }
+
+  submitMovie(){
+    this.moviesService.createMovie(this.movie).subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
+  }
 
 }
