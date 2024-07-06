776fc9b2f818c51dfad7d4ce93220ae2bf3d15f0
Jesus Sierra <sierraja06@gmail.com>
2024-07-06 08:58:54 -0500

    .

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule, RouterLink, RouterLinkActive, ],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, RouterLink, RouterLinkActive, FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
