f5bcce45037804758f1770823e021a661575d8e5
Jesus Sierra <sierraja06@gmail.com>
2024-07-07 12:24:06 -0500

    Add routerLink service

--- Frontend/src/app/components/movies-list/movies-list.component.ts
+++ Frontend/src/app/components/movies-list/movies-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { Movie } from '../../interfaces/movie';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-movies-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './movies-list.component.html',
   styleUrl: './movies-list.component.css'
 })
